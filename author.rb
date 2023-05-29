33f3e58ebd83436407c7e2d2f0ff9ca07811d3ba
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-05-29 10:18:59 +0300

    added comments

--- author.rb
+++ author.rb
@@ -4,19 +4,21 @@ class Author
     def initialize(name)
       @name = name
     end
-  
+ 
+    # Return an array of Article instances the author has written
+
     def articles
       Article.all.select { |article| article.author == self }
     end
-  
+  # Return a unique array of Magazine instances for which the author has contributed to
     def magazines
       articles.map { |article| article.magazine }.uniq
     end
-  
+  # Create a new Article instance and associates it with the author and magazine
     def add_article(magazine, title)
       Article.new(self, magazine, title)
     end
-  
+  # Return a unique array of strings with the categories of the magazines the author has contributed to
     def topic_areas
       magazines.map { |magazine| magazine.category }.uniq
     end
--- magazine.rb
+++ magazine.rb
@@ -12,19 +12,19 @@ class Magazine
     def self.all
       @@all
     end
-  
+  # Return an array of Author instances who have written for this magazine
     def contributors
       Article.all.select { |article| article.magazine == self }.map { |article| article.author }
     end
-  
+  # Return the first magazine object that matches the given name
     def self.find_by_name(name)
       @@all.find { |magazine| magazine.name == name }
     end
-  
+  # Return an array of strings of the titles of all articles written for this magazine
     def article_titles
       Article.all.select { |article| article.magazine == self }.map { |article| article.title }
     end
-  
+  # Return an array of authors who have written more than 2 articles for the magazine
     def contributing_authors
       contributors.group_by(&:itself).select { |_, authors| authors.length > 2 }.keys
     end
